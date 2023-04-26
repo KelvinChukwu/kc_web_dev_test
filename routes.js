b8d5845b6d3a2e14eccb6b619c20a1e16dfbab07
Kelvin Chukwu <kelvin.chukwu@mail.utoronto.ca>
2023-04-25 23:52:27 -0700

    backend step 1 complete

--- routes.js
+++ routes.js
@@ -1,7 +1,26 @@
 const routes = require('express').Router();
+const https = require('https');
 
 routes.get('/', (req, res) => {
-  res.send("Hello, World!");
+    res.send("Hello, World!");
+});
+
+routes.get('/api/psets', (req, res) => {
+    var url = 'https://www.orcestra.ca/api/psets/canonical';
+    https.get(url, (resp) => {
+        let data = '';
+
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            res.send(JSON.parse(data));
+        });
+
+    }).on("error", (err) => {
+        res.send("Error: " + err.message);
+    });
 });
 
 module.exports = routes;
