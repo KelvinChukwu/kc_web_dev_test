8b8bdce23ab82ad18cb9fd0a388ad7c9b9bc99c1
Kelvin Chukwu <kelvin.chukwu@mail.utoronto.ca>
2023-04-27 06:54:40 -0700

    now returning from rest api

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState, useEffect } from 'react';
+
+
+/**
+ * For psets just directly get name and doi
+ * for pset-databse, name can get directly, doi, need to go one level into repositories, take the first result but null check just in case
+*/
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/psets")
+      .then((res) => res.json())
+      .then((data) => setData(data.map(pSet => ({ name: pSet.name, doi: pSet.doi }))));
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{!data ? "Loading..." : data.map(pSet => <div>{pSet.name}</div>)}</p>
     </div>
   );
 }
